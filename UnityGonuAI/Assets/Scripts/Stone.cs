aa54c687e08defe0c74e3a13095fb469a0fb8b24
seongbae15 <pseongbae@gmail.com>
2022-11-29 17:03:59 +0900

    feat : create Stone Class

---
+++ UnityGonuAI/Assets/Scripts/Stone.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stone : MonoBehaviour
+{
+    private int position = -1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+}
